d25b13247e6b77219f0b094cf65bf299f4a49bb7
HeroNim <alexradvenis@gmail.com>
2014-06-14 14:06:33 -0700

    Level Design - Possibly Worse

---
+++ WatchYourBack/Components/TileComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WatchYourBack
+{
+    class TileComponent : EComponent
+    {
+        public readonly static int bitMask = (int)Masks.Tile;
+        public override int Mask { get { return bitMask; } }
+
+        
+      
+        
+    }
+}
